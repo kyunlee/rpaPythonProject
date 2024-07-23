4b268c16c85b09dc7c598402d649ddbbcbfebd26
kyunlee <kyunlee87@gmail.com>
2024-07-23 12:43:11 +0900

    Selenium

--- 0715.py
+++ 0715.py
@@ -1,3 +1,16 @@
-import pyautogui as p # as 뒤는 약자이다.
+import pyautogui as p 
+import time
+import selenium # 여기서는 약자를 붙이지 않았다.
+from selenium import webdriver
 
-p.position()
+#time.sleep(2) # 2초간 인터벌을 인위적으로 부여한다.
+#p.position() # 현재 x축과 y축의 좌표
+
+#Point(x = 1379, y= 97)
+
+#p.onScreen(300, 300)
+#p.moveTo(300, 300, duration=1)
+ 
+#p.alert(text='확인되엇습니다.', title = '인증', button='Ok')
+
+dirver = webdriver.Chrome('chromedriver.exe')
